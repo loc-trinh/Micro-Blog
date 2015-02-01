eb2b1c3afc5de3ed64f1d6a8b826bc6b55790964
Loc Trinh <loctrinh@mit.edu>
2015-01-31 20:34:38 -0500

    first commit

--- README.md
+++
@@ -1,4 +0,0 @@
-Micro-blog
-==========
-
-Web application written in Python using Flask development framework.
--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-DEBUG = True
-SECRET_KEY = 'top secret key'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
